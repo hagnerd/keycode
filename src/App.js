091f2ed90e5f295124ad1c446cd5c72e93974fcc
Matt Hagner <web@matthagner.com>
2018-08-23 22:20:47 -0500

    Refactor app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import styled, { keyframes } from 'react-emotion';
 import { injectGlobal } from 'emotion';
 
 import Card from './components/Card';
 import Display from './components/Display';
+import PlaceHolder from './components/PlaceHolder';
 import KeyListener from './containers/KeyListener';
+import { CardsWrapper, FlexColumn } from './ui';
 
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
@@ -18,43 +19,13 @@ injectGlobal`
   }
 `;
 
-const CardsWrapper = styled('div')`
-  display: flex;
-  justify-content: space-around;
-`;
-
-const FlexColumn = styled('div')`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 100vh;
-  width: 100vw;
-`;
-
-const pulse = keyframes`
-  0%, 100% {
-    opacity: 0.5;
-    transform: scale(0.8);
-  }
-
-  50% {
-    opacity: 1;
-    transform: scale(1);
-  }
-`;
-
-const PlaceHolder = styled('h1')`
-  text-align: center;
-  animation: ${pulse} 3s infinite;
-`;
-
 const App = () => (
   <KeyListener>
     {({ code, key, which, keyCode }) => {
       if (keyCode === '-') {
         return (
           <FlexColumn>
-            <PlaceHolder>Press Any Key</PlaceHolder>
+            <PlaceHolder>Press any key</PlaceHolder>
           </FlexColumn>
         );
       }
